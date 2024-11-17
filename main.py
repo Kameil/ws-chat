ad019acde9ad26ef724bcde94a5f4ac62b10da50
kameil <racovikpvpbr@gmail.com>
2024-11-17 15:41:28 -0300

    melhorias na seguranca do websocket

--- main.py
+++ main.py
@@ -19,20 +19,18 @@ async def echo(websocket: websockets.legacy.server.WebSocketServerProtocol):
     connected_clients.add(websocket)
     try:
         async for message in websocket:
-            # Cria tarefas para enviar a mensagem a todos os clientes conectados
             tasks = [client.send(message) for client in connected_clients]
-            await asyncio.gather(*tasks)  # Aguarda todas as tarefas
+            await asyncio.gather(*tasks)
     except Exception as e:
         print(f"Um erro ocorreu: {e}")
     finally:
-        # Remove o cliente da lista quando a conexão é fechada
         connected_clients.remove(websocket)
 
 async def main():
     print("iniciado")
     async with serve(echo, "0.0.0.0", 8765):
-        await asyncio.get_running_loop().create_future()  # run forever
+        await asyncio.get_running_loop().create_future()
 
 
-# threading.Thread(target=flaskserver).start()
+
 asyncio.run(main())
